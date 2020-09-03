95e80f44084076152c50afe9fb36c4ca2798eb98
kolyan.samokhin@mail.ru <36818428+NikolaysSamokhin@users.noreply.github.com>
2020-09-03 15:32:43 +0300

    Update code

--- swagger-handler/src/main/java/swagger_handler/utils/AbstractSwaggerVerification.java
+++ swagger-handler/src/main/java/swagger_handler/utils/AbstractSwaggerVerification.java
@@ -2,31 +2,6 @@ package swagger_handler.utils;
 
 import swagger_handler.interfaces.SwaggerProcessing;
 
-import java.io.FileInputStream;
+public abstract class AbstractSwaggerVerification implements SwaggerProcessing {
 
-public class AbstractSwaggerVerification implements SwaggerProcessing {
-    @Override
-    public String deleteJsonDeprecatedEndpoints(String swagger) {
-        return null;
-    }
-
-    @Override
-    public String deleteYamlDeprecatedEndpoints(String swagger) {
-        return null;
-    }
-
-    @Override
-    public String getContentFile(FileInputStream fis) {
-        return null;
-    }
-
-    @Override
-    public String deleteIncorrectEndpoints(String swagger) {
-        return null;
-    }
-
-    @Override
-    public String verificateJsonSwagger(String swagger) {
-        return null;
-    }
 }
